57279beff189e51f12656c25f2e21f638e94acec
Mansha <manshapreet02@gmail.com>
2024-04-13 17:40:58 +0530

    nav mobile ht

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,26 +11,11 @@ const About = ({about_ref}) => {
         <div className='heading'>About Us</div>
         <Line/>
         <div className='about_content'>
-            <div>Bhurji Electronics has been the leading transformer 
-                manufacturer in the world since its establishment in 1972.
-                 Bhurji has evolved over the past 5 decades from being the 
-                 75% Indian market shareholder of TV transformers to its 
-                 state-of-the-art products curated and developed to serve 
-                 the global energy needs.
+            <div>Bhurji Electronics has been the leading transformer manufacturer in India since its establishment in 1972. Bhurji has evolved over the past 5 decades from being the 75% Indian market shareholder of TV transformers to its state-of-the-art efficient products curated and developed to serve the national energy needs.
                  <br /><br />
-
-
-With the futuristic approach towards innovation and research, Bhurji 
-Electronics led the markets as the first F.R. Transformers developers. We 
-have successfully delivered constant voltage transformers to multiple
- governmental organizations and OEM’s such as The Indian Railways, Ministry 
- of Finance, Income Tax Department, HCL Limited, WIPRO Limited and SIEMENS Limited.
-
-<br /><br />
-Today we have a complete backward manufacturing facility in Gurugram, India 
-which holds the capabilities to facilitate progressive tools, die requirements
- for lamination cutting and punching and developing transformer clamps, studs
-  and housing cabinets for its captive usage.</div>
+            With the futuristic approach towards innovation and research, Bhurji Electronics led the markets as the first F.R. Transformers developers. We have successfully delivered constant voltage transformers to multiple governmental organizations and OEM’s such as The Indian Railways, Ministry of Finance, Income Tax Department, HCL Limited, WIPRO Limited and SIEMENS Limited.
+            <br /><br />
+            Today we have a complete backward manufacturing facility in Gurugram, India which holds the capabilities to facilitate progressive tools, die requirements for lamination cutting and punching and developing transformer clamps, studs and housing cabinets for its captive usage, as well.</div>
             <div className='about_img_div'><img className='about_img' src={about_img} /></div>
         </div>
     </div>
